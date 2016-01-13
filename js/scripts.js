feb876bcc2a4769ebf80558feeb2ededfe250493
Anna Becker and Taylor Pokoj <anna.r.becker@gmail.com>
2016-01-13 14:01:51 -0800

    Fix javascript show results function and add images.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,35 @@
 $(document).ready(function() {
-  $("form#questions").submit(function(event){
-    var food = $("select#favoriteFood").val();
+  $("form#questions").submit(function(event) {
     var age = $("input#age").val();
     var gender = $("select#genderPreference").val();
-    var interests = $("select#interests").val();
+    //
+    // var result = (age && gender);
 
-    var result = (food + age + gender + interests);
-      if (food === 'Italian') {
-        result = 
-      }
-  }
+    if (age <= 39 && gender === 'male') {
+      $("#lebron").show();
+    }
+    else if (age <= 39 && gender === 'female') {
+      $("#katy").show();
+    }
+    else if (age <= 39 && gender === 'either') {
+      $("#katy").show();
+      $("#lebron").show();
+    }
+    else if (age >= 40 && gender === 'male') {
+      $("#clooney").show();
+    }
+    else if (age >= 40 && gender === 'female') {
+      $("#hilary").show();
+    }
+    else if (age >= 40 && gender === 'either') {
+      $("#clooney").show();
+      $("#hilary").show();
+    }
+    else {
+      alert("Please answer all questions!")
+    }
+    //
+    // $("#result").show();
+    event.preventDefault();
+  });
 });
